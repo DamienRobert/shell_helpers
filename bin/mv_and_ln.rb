79edeb887607dca8931c7dfbae355a51a395eb7c
Damien Robert <damien.olivier.robert+git@gmail.com>
2015-04-26 18:37:14 +0200

    mv_and_ln.rb: dereference

--- bin/mv_and_ln.rb
+++ bin/mv_and_ln.rb
@@ -5,9 +5,11 @@ require 'shell_helpers/options'
 
 opts = Slop.parse(ARGV) do |o|
 	o.bool "-v", "--verbose", "verbose"
+	o.int "--verbose-level", "verbose level"
 	o.bool "-t", "--test", "test"
 	o.symbol "-m", "--mode", "Conversion mode", default: :rel
 	o.symbol "--rm", "rm options", default: :noclobber
+	o.symbol "--dereference-mode", "dereference mode for dest"
 	o.bool "-L", "--dereference", "dereference dest"
 	o.bool "-f", "--force", "force remove"
 	o.on '--help' do
@@ -15,6 +17,14 @@ opts = Slop.parse(ARGV) do |o|
 		exit
 	end
 end
+
+if opts[:force]
+	opts[:rm] ||= :all 
+else
+	opts[:rm] ||= :noclobber
+end
+#dereference-mode can also take the values 'simple' and 'none'
+opts[:"dereference-mode"] ||= opts[:dereference]
 
 pathname=ShellHelpers::Pathname
 if opts[:verbose] and opts[:test]
@@ -26,6 +36,7 @@ elsif opts[:test]
 end
 
 *args,dest=opts.args
+dest=pathname.new(dest)
 if args.empty?
 	warn "You should specify at least two arguments"
 	exit 1
@@ -34,11 +45,10 @@ if args.length > 1 and !dest.directory?
 	warn "When specifying more than one target, dest should be a directory"
 	exit 1
 end
-dest=pathname.new(dest)
 args.each do |f|
 	f=pathname.new(f)
 	t=dest.directory? ? dest+f.basename : dest
 	t=f.rel_path_to(t, mode: opts[:mode])
 	puts "#{f} -> #{t}" if opts[:verbose]
-	dest.on_mv(f, mode: opts[:rm], verbose: opts[:verbose]) and f.on_ln_s(t, verbose: opts[:verbose])
+	dest.on_mv(f, mode: opts[:rm], verbose: opts[:verbose], dereference: opts[:"dereference-mode"]) and f.on_ln_s(t, verbose: opts[:verbose])
 end
