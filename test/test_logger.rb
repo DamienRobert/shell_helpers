a5d0d41e0f0faf0bffa017ba048ac9dfff2819e8
Damien Robert <damien.olivier.robert+git@gmail.com>
2019-04-30 10:26:13 +0200

    Fix tests

--- test/test_logger.rb
+++ test/test_logger.rb
@@ -1,40 +1,39 @@
 require 'helper'
 require 'shell_helpers'
 
-describe ShellHelpers::MoreLogger do
+describe ShellHelpers::ColorLogger do
 	before do
 		@buffer=StringIO.new
-		@logger=SH::MoreLogger.new(@buffer)
+		@logger=SH::ColorLogger.new(@buffer)
 		@logger.formatter=SH::CLILogger::BLANK_FORMAT
 	end
-	it "Has a color info mode" do
-		@logger.cli_info "foo"
-		# @buffer.string.must_equal "\e[1mfoo\e[0m\n"
+	it "Has a info mode" do
+		@logger.info "foo"
 		@buffer.string.must_equal "foo\n"
 	end
 	it "Has a color mark mode" do
-		@logger.cli_mark "foo"
+		@logger.mark "foo"
 		@buffer.string.must_equal "\e[1mfoo\e[0m\n"
 	end
 	it "Has a colored important mode" do
-		@logger.cli_important "foo"
+		@logger.important "foo"
 		@buffer.string.must_equal "\e[34;1mfoo\e[0m\n"
 	end
 	it "The colored mode can add colors" do
-		@logger.cli_important "foo", color: [:red]
+		@logger.important "foo", color: [:red]
 		@buffer.string.must_equal "\e[34;1;31mfoo\e[0m\n"
 	end
-	it "Has an important mode" do
-		@logger.important "foo"
+	it "Has a raw important mode" do
+		@logger.important "foo", raw: true
 		@buffer.string.must_equal "foo\n"
 	end
 	it "Can give a numeric level" do
-		@logger.cli_add(@logger.severity(:important), "foo")
+		@logger.add(@logger.severity(:important), "foo")
 		@buffer.string.must_equal "foo\n"
 	end
-	it "Default to debug level" do
+	it "Default to info level" do
 		@logger.debug("foo")
-		@buffer.string.must_equal "foo\n"
+		@buffer.string.must_equal ""
 	end
 	it "Don't show below debug level" do
 		@logger.debug3("foo")
